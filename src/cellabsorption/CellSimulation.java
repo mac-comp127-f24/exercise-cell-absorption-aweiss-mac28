7305b8da9fef43e8bc96f54b14458e29c86f9557
aweiss-mac28 <aweiss1@macalester.edu>
2024-10-02 10:33:31 -0500

    Added cellInteraction and handleCellInteraction

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -30,7 +30,7 @@ public class CellSimulation {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             for(Cell cell:cells){
             cell.moveAround(canvasCenter);
-            cell.grow(0.02);
+            handleCellInteraction();
 
             }
             
@@ -54,6 +54,16 @@ public class CellSimulation {
 
     private static double sqr(double x) {
         return x * x;
+    }
+
+    private void handleCellInteraction(){
+        for(int i = 0;i<cells.size();i++){
+            Cell cell1 = cells.get(i);
+            for(int j = i+1;j<cells.size();j++){
+                Cell cell2  =cells.get(j);
+                cell1.interactWith(cell2);
+            }
+        }
     }
 
 }
